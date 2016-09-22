faa0ad271ce00eeb8dd6a52bfdf81fb21955213b
Dews <sam.mamane@hotmail.fr>
2016-09-22 16:43:21 +0200

    Exercice 2
    Reussi

--- TP1/ADN.java
+++ TP1/ADN.java
@@ -1,5 +1,30 @@
+import java.util.*;
 class ADN {
     public static char baseADN(){
-        
+        int a = (int)(0 + Math.random()*10/3);
+        switch(a){
+            case 0 : {
+                return 'A';
+            }
+            case 1 : {
+                return 'C';
+            }
+            case 2 : {
+                return 'G';
+            }
+            case 3: {
+                return 'T';
+            }
+        }
+        return 'a';
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = "";
+        int d = sc.nextInt();
+        for(int i = 0; i<d;i++){
+            a = a+Character.toString(baseADN());
+        }
+        System.out.println(a);
     }
 }
