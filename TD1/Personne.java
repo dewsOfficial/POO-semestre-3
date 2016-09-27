0fcd2165222cffa63ae005a32d03c0fc05829743
Yapoto <sam.mamane@hotmail.fr>
2016-09-27 17:52:29 +0200

    Exercice 3
    Fin de l’exo 3

--- TD1/Personne.java
+++ TD1/Personne.java
@@ -16,4 +16,16 @@ public class Personne {
     public String toString(){
         return "Je m'appelle : " +this.prenom+" " +this.nom+ " J'ai "+ this.age+" ans.";
     }
+    public int monnaie;
+
+    
+    public boolean donne(Personne p, int montant){
+        if(montant<=this.monnaie){
+            this.monnaie = this.monnaie-montant;
+            p.monnaie = p.monnaie+montant;
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
--- TD1/Test.java
+++ TD1/Test.java
@@ -2,8 +2,27 @@ public class Test{
     public static void main(String [] args){
         Personne tony = new Personne ("Parker","Tony",29);
         System.out.println(tony);
-        Personne mickael = tony;
+        Personne mickael = new Personne("Tony","Gelabale",30);
         mickael.setPrenom("Mickael");
         System.out.println(mickael);
+        mickael.monnaie = 80;
+        tony.monnaie = 30;
+        //tony donne 20 a mickael
+        boolean a = donne(tony,mickael,20);
+        System.out.println(mickael.monnaie);
+        System.out.println(tony.monnaie);
+        //mickael lui rends
+        boolean b = mickael.donne(tony,20);
+        System.out.println(mickael.monnaie);
+        System.out.println(tony.monnaie);
+    }
+    public static boolean donne(Personne p1, Personne p2, int montant){
+        if(montant<=p1.monnaie){
+            p1.monnaie = p1.monnaie-montant;
+            p2.monnaie = p2.monnaie+montant;
+            return true;
+        }else{
+            return false;
+        }
     }
 }
